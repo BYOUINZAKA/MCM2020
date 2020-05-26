01eea38e43d7d51a5197dbaf0ddbc800889b5ec0
BYOUINZAKA <2606675531@qq.com>
2020-05-27 01:53:24 +0800

    5.27更新

--- templates/forecast.py
+++ templates/forecast.py
@@ -2,7 +2,7 @@
 @Author: Hata
 @Date: 2020-05-26 16:33:47
 @LastEditors: Hata
-@LastEditTime: 2020-05-26 22:46:53
+@LastEditTime: 2020-05-27 15:07:32
 @FilePath: \MCM2020\templates\forecast.py
 @Description:
 '''
@@ -19,7 +19,7 @@ class GrayForecast:
         if type(data) is np.ndarray:
             self.__data = data
         else:
-            self.__data = np.asarray(data)
+            self.__data = np.array(data)
 
     def Check(self, c=0) -> bool:
         data = self.__data+c
@@ -32,25 +32,27 @@ class GrayForecast:
         self.__c = c
         return True
 
-    def Build(self, choice=0) -> callable:
+    def Build(self, choice: int) -> callable:
         data = self.__data[choice:]+self.__c
         x_1 = np.array([np.sum(data[:i+1]) for i in range(len(data))])
-        z_1 = np.array([-0.5*(x_1[i-1]+x_1[i]) for i in range(1, len(data))])
-        B = np.vstack((-z_1, np.ones(len(z_1)))).T
+        z_1 = np.array([0.5*(x_1[i-1]+x_1[i]) for i in range(1, len(data))])
+        B = np.vstack((z_1, np.ones(len(z_1)))).T
         Y = np.array(data[1:])
         a, b = np.abs((np.linalg.inv(B.T@B) @ B.T) @ Y)
 
         return lambda k: (data[0]-b/a)*(1-np.exp(a))*np.exp(-a*k)-self.__c
 
-    def Generate(self, k: int, choice=0) -> np.ndarray:
-        generate = self.Build(choice=choice)
-        return np.array([generate(_k) for _k in range(1, k)])
+    def Generate(self, k: int, choice=0, generator=None) -> np.ndarray:
+        if generator is None:
+            generator = self.Build(choice=choice)
+        return np.array([generator(_k) for _k in range(1, k)])
 
-    def Verify(self, genlist) -> dict:
-        maxlen = min(len(self.__data), len(genlist))
+    def Verify(self, genList) -> dict:
+        genlist = np.hstack((0, genList))   
+        minlen = min(len(self.__data), len(genlist))
         e = [self.__data[i]-genlist[i]
-             for i in range(1, maxlen)]
-        delta = np.abs(e/self.__data[1:maxlen])
+             for i in range(1, minlen)]
+        delta = np.abs(e/self.__data[1:minlen])
         xavg = self.__data.sum()/len(self.__data)
         eavg = np.sum(e)/len(e)
         s_1 = np.array([(xk - xavg)**2 for xk in self.__data]
@@ -61,15 +63,12 @@ class GrayForecast:
             'S_1': s_1,
             'S_2': s_2,
             'deltas': delta,
-            'delta_avg': delta.sum()/(maxlen-1)
+            'delta_avg': delta.sum()/(minlen-1)
         }
 
 
 if __name__ == "__main__":
-    data = [33.27, 43.41, 62.06, 101.72, 131.15, 170.73,
-            217.69, 296.39, 440.69, 457.12, 559.11]
-
-    data = [71.1, 72.4, 72.4, 72.1, 71.4, 72, 71.6, 71.0, 72]
+    data = [71.1, 72.4, 72.4, 72.1, 71.4, 72, 71.6, 71.0, 70]
     plt.subplot(211)
     plt.title('X_0')
     plt.scatter(np.arange(len(data)), data)
@@ -83,6 +82,7 @@ if __name__ == "__main__":
 
     plt.subplot(212)
     plt.title('X_1')
+
     x_1 = np.array([np.sum(data[:i+1]) for i in range(len(data))])
     g_1 = np.array([np.sum(lst[:i+1]) for i in range(len(lst))])
     plt.scatter(np.arange(len(x_1)), x_1)
@@ -91,11 +91,3 @@ if __name__ == "__main__":
     plt.tight_layout()
 
     plt.show()
-
-{
-    'C': 0.5158581516842807,
-    'S_1': 0.2461728395061746,
-    'S_2': 0.12699026598252633,
-    'deltas': array([0.00301566, 0.00478461, 0.00241688, 0.00557581, 0.00457356, 0.00078892, 0.00586681, 0.009864]),
-    'delta_avg': 0.004610781318595852
-}
--- templates/数理统计.py
+++ templates/数理统计.py
@@ -2,11 +2,12 @@
 @Author: Hata
 @Date: 2020-05-25 17:03:42
 @LastEditors: Hata
-@LastEditTime: 2020-05-26 16:26:33
+@LastEditTime: 2020-05-27 19:28:37
 @FilePath: \MCM2020\templates\数理统计.py
 @Description: 
 '''
 
+from sklearn import linear_model
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy import stats, integrate
@@ -33,6 +34,9 @@ def CDFProgram():
 
 
 def Chisquare():
+    '''
+    @description: 例7-8
+    '''
     def func(t):
         if t >= 0:
             return 0.005*np.exp(-0.005*t)
@@ -42,8 +46,65 @@ def Chisquare():
     obs = np.array([121, 78, 43, 58])
     exp = np.array([integrate.quad(func, edges[i-1], edges[i])[0]*300
                     for i in range(1, len(edges))])
-    print(stats.chisquare(obs, exp))
+    return stats.chisquare(obs, exp)
+
+
+def Regression():
+    '''
+    @description: 例7-19
+    '''
+    y = np.zeros((25, 1))
+    x = np.zeros((25, 3))
+    with open('.\\references\\司守奎 《数学建模算法与应用》\\【代码】司守奎 《数学建模算法与应用》 第二版\\07第7章 数理统计\\ex7_19.txt') as file:
+        for line in file:
+            array = line.rstrip().split('  ')[0].split('\t')
+            i = 0
+            while i < len(array):
+                if i == 0 or i == 5:
+                    index = int(array[i])-1
+                    i = i+1
+                    continue
+                y[index, 0] = float(array[i])
+                x[index, ] = [float(array[i+1]),
+                              float(array[i+2]), float(array[i+3])]
+                i = i + 4
+    plt.grid()
+    model = linear_model.LinearRegression()
+    model.fit(x, y)
+    pltx = np.arange(1, 26)
+    plt.scatter(pltx, y.T[0], marker='*')
+    plt.scatter(pltx, model.predict(x).T[0], marker='.')
+    return model.intercept_, model.coef_
+    # result = (array([0.8538775]), array([[0.0177632 , 2.07820655, 1.93958733]]))
+
+
+def Regression2():
+    '''
+    @description: 7-5
+    '''    
+    x = np.array([1, 2, 4, 5, 7, 8, 9, 10])
+    y = np.array([1.3, 1, 0.9, 0.81, 0.7, 0.6, 0.55, 0.4])
+    x_t = np.vstack((1/x, x, x**2))
+    y.shape = (len(y), 1)
+    plt.grid()
+    model = linear_model.LinearRegression()
+    model.fit(x_t.T, y)
+
+    def func(x):
+        a1, a3, a4 = model.coef_[0]
+        a2 = model.intercept_[0]
+        return a1/x + a2 + a3*x + a4*x**2
+
+    plt.scatter(x, y.T[0], marker='*', c='black')
+    px = np.linspace(x[0]-0.5, x[-1]+0.5, 100)
+    py = func(px)
+    plt.plot(px, py)
+
+    return "y={0:.2f}/x+{1:.2f}+{2:.2f}*x+{3:.2f}*x**2".format(model.coef_[0][0], model.intercept_[0], model.coef_[0][1], model.coef_[0][2])
+    # result = "y=0.65/x+0.59+0.07*x+-0.01*x**2"
 
 
 # print(Interval())
-Chisquare()
+# Chisquare()
+print(Regression2())
+plt.show()
