e3cceeb4c79fc6f155a973f0091fbf5516861afd
BYOUINZAKA <2606675531@qq.com>
2020-06-05 18:43:18 +0800

    修改A-1

--- code/anotherA/solve1.py
+++ code/anotherA/solve1.py
@@ -3,7 +3,7 @@
 @Date: 2020-06-04 23:37:44
 @LastEditors: Hata
 <<<<<<< HEAD
-@LastEditTime: 2020-06-05 18:14:55
+@LastEditTime: 2020-06-05 18:42:59
 =======
 @LastEditTime: 2020-06-05 12:39:01
 >>>>>>> 7c0d5a655a4619ca1f5c1239af30c76d5c96e0db
@@ -19,7 +19,6 @@ import dataform as dfm
 
 if __name__ == "__main__":
     center = 0
-<<<<<<< HEAD
     df = dfm.DataForm(".\\code\\anotherA\\data.csv")
 
     # 构筑一级水站和二级水站间的最小生成树。
@@ -29,26 +28,9 @@ if __name__ == "__main__":
     G.add_edge(*for_add[:2], weight=for_add[2])
 
     # 绘制
-=======
-
-    df = dfm.DataForm(".\\code\\anotherA\\data.csv")
-    G = nx.minimum_spanning_tree(df.BuildGraph('V|P'), algorithm='prim')
-    centralNeighbors = df.BuildGraph('A|V')[center]
-
-    minWeight = np.Infinity
-    for key in centralNeighbors:
-        if centralNeighbors[key]['weight'] < minWeight:
-            minWeight = centralNeighbors[key]['weight']
-            target = key
-    G.add_edge(center, target, weight=minWeight)
-
->>>>>>> 7c0d5a655a4619ca1f5c1239af30c76d5c96e0db
     dfm.graphDraw(df, G)
     df.Draw()
     print("管道总长为%.2fm，其中一级管道总长%.2fm，二级管道总长%.2fm。（结果保留两位小数）"
           % dfm.weightStats(df, G))
-<<<<<<< HEAD
     plt.savefig('.\\code\\anotherA\\result\\solve1.png')
-=======
->>>>>>> 7c0d5a655a4619ca1f5c1239af30c76d5c96e0db
     plt.show()
